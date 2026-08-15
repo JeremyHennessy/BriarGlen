056afa85f1cc92d87eee52caf1ed9af748239803
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-15 18:17:49 -0300

    Add Build 15 release metadata

---
+++ src/v15/23-release-info.js
@@ -0,0 +1,17 @@
+(() => {
+  'use strict';
+
+  const BUILD_INFO = Object.freeze({
+    version: '15',
+    label: 'Enemy & Combat Identity',
+    saveKey: 'briar-glen-vslice-v1',
+    schema: 1,
+  });
+
+  window.__BRIAR_GLEN_BUILD = BUILD_INFO;
+  document.documentElement.dataset.briarGlenBuild = BUILD_INFO.version;
+
+  if (window.__BRIAR_GLENDebug) {
+    window.__BRIAR_GLENDebug.getBuildInfo = () => ({ ...BUILD_INFO });
+  }
+})();
