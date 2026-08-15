048e1e4a5792e6aa5e2d766f03a52f9151d68934
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-15 18:34:01 -0300

    Add Build 16 release metadata

---
+++ src/v16/25-release-info.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const BUILD_INFO = Object.freeze({ version: '16', label: 'Art & World Identity', saveKey: 'briar-glen-vslice-v1', schema: 1 });
+  window.__BRIAR_GLEN_BUILD = BUILD_INFO;
+  document.documentElement.dataset.briarGlenBuild = BUILD_INFO.version;
+  if (window.__BRIAR_GLENDebug) window.__BRIAR_GLENDebug.getBuildInfo = () => ({ ...BUILD_INFO });
+})();
