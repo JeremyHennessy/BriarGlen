0b92bab5c9947b40d223852451c084ef10dacbe9
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-16 14:11:34 -0300

    Set Build 23 release metadata

--- src/runtime/release-info.js
+++ src/runtime/release-info.js
@@ -1,8 +1,8 @@
 (() => {
   'use strict';
   const BUILD_INFO = Object.freeze({
-    version:'22',
-    label:'Balance & Pacing',
+    version:'23',
+    label:'Vertical Slice Polish',
     saveKey:'briar-glen-vslice-v1',
     schema:1,
     runtime:'canonical-manifest-hooks-v1',
