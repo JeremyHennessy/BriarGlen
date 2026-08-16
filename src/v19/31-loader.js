35ea3f5f683e2dff753e7891967eacc639572b54
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-16 12:17:23 -0300

    Chain Build 20 loader after Build 19

--- src/v19/31-loader.js
+++ src/v19/31-loader.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  for (const src of ['src/v19/31-biome-art.js','src/v19/32-release-info.js']) {
+  for (const src of ['src/v19/31-biome-art.js','src/v19/32-release-info.js','src/v20/33-loader.js']) {
     const script = document.createElement('script');
     script.src = src;
     script.async = false;
