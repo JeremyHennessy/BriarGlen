6f20f93e7cd30a778c6b972dc64ceb1dc93e7ffe
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-18 18:50:43 -0300

    Build 38: deepen specialist economy

--- src/runtime/release-info.js
+++ src/runtime/release-info.js
@@ -1,8 +1,8 @@
 (() => {
   'use strict';
   const BUILD_INFO = Object.freeze({
-    version:'37',
-    label:'Contextual Onboarding',
+    version:'38',
+    label:'Specialist Economy',
     saveKey:'briar-glen-vslice-v1',
     schema:1,
     runtime:'canonical-manifest-hooks-v1',
